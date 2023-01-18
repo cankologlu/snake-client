dc4e4c371f0bc1f75b5e0dd9c54b6d3cefb6497a
Can Kologlu <mckologlu@gmail.com>
2023-01-18 16:50:13 -0500

    client file for connection and motion upwards

---
+++ client.js
@@ -0,0 +1,34 @@
+const net = require("net");
+
+// establishes a connection with the game server
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here
+    port: "50541"  // Port number here,
+  
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Ready to play");
+
+    conn.write("Name: MCK")
+    // conn.write("Move: up")
+
+
+  });
+
+conn.on("data", (data) => {
+  console.log(`Server says: `, data)
+});
+
+
+  return conn;
+
+  
+};
+
+module.exports = connect;
